d4c5a732aacc9463fd6e97ed328456016833b1df
James Gould <nobleburgundy@gmail.com>
2020-11-25 11:12:33 -0600

    Added more questions.

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+// Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 const questions = [
   {
     type: "input",
-    name: "repository_name",
-    message: "What is the name of your repository?",
+    name: "title",
+    message: "What is the title of your application?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please enter a description: ",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Enter the installation instructions: ",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Usage: ",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "Contribution Guidelines: ",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "How to run tests: ",
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "Your github username: ",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Email address: ",
   },
 ];
 
